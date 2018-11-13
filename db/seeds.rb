2a3e64cc9a258acf2ddf7803b62314ccdc41389a
rawan-1 <rawan.cs18@gmail.com>
2018-11-13 18:23:00 +0300

    editing NewAppliance

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
+# User.destroy_all
 
-user1 = User.create(name: "Rawan", location: "Riyadh", email: "sjdmc@dk.com", password: "chicken", password_confirmation: "chicken")
-user2 = User.create(name: "Moath", location: "Riyadh", email: "moath@dk.com", password: "chicken", password_confirmation: "chicken")
+# user1 = User.create(name: "Rawan", location: "Riyadh", email: "sjdmc@dk.com", password: "chicken", password_confirmation: "chicken")
+# user2 = User.create(name: "Moath", location: "Riyadh", email: "moath@dk.com", password: "chicken", password_confirmation: "chicken")
 
-Appliance.destroy_all
+# Appliance.destroy_all
 
-Appliance.create(name: "Mixer", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQXYbI4u6Jk6bvo-Jr6lNKdVG4on5djmnIKRB8PVu5oVyNIC91xA", description: "KitchenAid Tilt-Head Stand Mixer
-4-1/2 quart and stainless steel bowl w/flat handle", availability: "true", customer_id: user1.id, provider_id: user2.id)
+# Appliance.create(name: "Mixer", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQXYbI4u6Jk6bvo-Jr6lNKdVG4on5djmnIKRB8PVu5oVyNIC91xA", description: "KitchenAid Tilt-Head Stand Mixer
+# 4-1/2 quart and stainless steel bowl w/flat handle", availability: "true", customer_id: user1.id, provider_id: user2.id)
 
-Appliance.create(name: "Mixer1", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQXYbI4u6Jk6bvo-Jr6lNKdVG4on5djmnIKRB8PVu5oVyNIC91xA", description: "KitchenAid Tilt-Head Stand Mixer
-4-1/2 quart and stainless steel bowl w/flat handle", availability: "true", customer_id: user1.id, provider_id: user2.id)
+# Appliance.create(name: "Mixer1", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQXYbI4u6Jk6bvo-Jr6lNKdVG4on5djmnIKRB8PVu5oVyNIC91xA", description: "KitchenAid Tilt-Head Stand Mixer
+# 4-1/2 quart and stainless steel bowl w/flat handle", availability: "true", customer_id: user1.id, provider_id: user2.id)
 
-Appliance.create(name: "Mixer3", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQXYbI4u6Jk6bvo-Jr6lNKdVG4on5djmnIKRB8PVu5oVyNIC91xA", description: "KitchenAid Tilt-Head Stand Mixer
-4-1/2 quart and stainless steel bowl w/flat handle", availability: "true", customer_id: user1.id, provider_id: user2.id)
+# Appliance.create(name: "Mixer3", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQXYbI4u6Jk6bvo-Jr6lNKdVG4on5djmnIKRB8PVu5oVyNIC91xA", description: "KitchenAid Tilt-Head Stand Mixer
+# 4-1/2 quart and stainless steel bowl w/flat handle", availability: "true", customer_id: user1.id, provider_id: user2.id)
