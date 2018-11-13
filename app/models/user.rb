9caeb581afd8682407364e18377a481ace951c91
RAWAN <rawan@RAWANs-MacBook-Pro.local>
2018-11-13 12:12:04 +0300

    Worked on grid system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :appliances, class_name: "Appliance", foreign_key: "provider_id"
+  has_many :provided_appliances, class_name: "Appliance", foreign_key: "provider_id"
   has_many :appliances, class_name: "Appliance", foreign_key: "customer_id"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -14,3 +14,6 @@ class User < ApplicationRecord
     user_type == "Customer"
   end
 end
+
+# rawan.provided_appliances # All appliances where provider_id is equal to your ID
+# rawan.appliances # All appliances where customer_id is equal to your ID
